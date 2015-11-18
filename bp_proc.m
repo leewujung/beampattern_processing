0d054c6ff35ac48d02f71e70fc8bff6a765c7fce
leewujung <leewujung@gmail.com>
2015-11-18 13:19:21 -0500

    Minor change in fcn load_bat_pos

--- bp_proc.m
+++ bp_proc.m
@@ -270,12 +270,23 @@ g = normpdf(-sm_len:sm_len,0,sm_len);
 w = conv(double(notnan),g,'same');
 idx = find(diff(w~=0)~=0)+1;
 idx_up = find(diff(w~=0)>0)+1;
+idx_dn = find(diff(w~=0)<0)+1;
 
-[~,iconv] = min(abs(repmat(idx',length(idx_nan),1)-repmat(idx_nan,1,length(idx))),[],1);
-if idx_up(1)~=idx(1)  % if the first up edge not at the beginning
-    seg_idx = [1;idx_nan(iconv)];
+if ~isempty(idx)
+    [~,iconv] = min(abs(repmat(idx',length(idx_nan),1)-repmat(idx_nan,1,length(idx))),[],1);
+    if isempty(idx_up) && ~isempty(idx_dn)
+        seg_idx = [1;idx_nan(iconv)];
+    elseif ~isempty(idx_up) && isempty(idx_dn)
+        seg_idx = [idx_nan(iconv);size(pos,1)];
+    else
+        if idx_up(1)~=idx(1)  % if the first up edge not at the beginning
+            seg_idx = [1;idx_nan(iconv)];
+        else
+            seg_idx = idx_nan(iconv);
+        end
+    end
 else
-    seg_idx = idx_nan(iconv);
+    seg_idx = [1,size(pos,1)];
 end
 if mod(length(seg_idx),2)~=0  % if the last down edge not at the end
     seg_idx = [seg_idx;length(notnan)];
