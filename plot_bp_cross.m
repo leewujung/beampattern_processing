d11c6e60e86acbd303384756bfa4c54e3407fe5c
leewujung <leewujung@gmail.com>
2016-04-22 23:02:44 -0700

    Fixed azimuth left/right flipped issue. Also make the polar display nicer with mic channel number plotted.

--- plot_bp_cross.m
+++ plot_bp_cross.m
@@ -46,10 +46,16 @@ az = mic_to_bat_angle(az_idx,1);  % azimuth
 el = mic_to_bat_angle(el_idx,2);  % elevation
 if strcmp(gui_op.linlog,'rb_lin')  % log/linear display
     az_amp = 10.^(call_dB(az_idx)/10);
+    az_amp = az_amp/min(az_amp);
     el_amp = 10.^(call_dB(el_idx)/10);
+    el_amp = el_amp/min(el_amp);
 else
     az_amp = call_dB(az_idx);
     el_amp = call_dB(el_idx);
+    az_amp = az_amp-max(az_amp)+30;
+    az_amp(az_amp<0) = NaN;
+    el_amp = el_amp-max(el_amp)+30;
+    el_amp(el_amp<0) = NaN;
 end
 
 az_plot = [az,az_amp(:),find(az_idx)];
@@ -57,10 +63,35 @@ az_plot = sortrows(az_plot,1);
 el_plot = [el,el_amp(:),find(el_idx)];
 el_plot = sortrows(el_plot,1);
 
+mic_num_seq = 1:length(az_idx);
+if sum(el_idx)
+    mic_el = mic_num_seq(el_idx);
+    [el_x,el_y] = pol2cart(el_plot(:,1),el_plot(:,2));
+else
+    mic_el = [];
+    el_x = [];
+    el_y = [];
+end
+if sum(az_idx)
+    mic_az = mic_num_seq(az_idx);
+    [az_x,az_y] = pol2cart(az_plot(:,1),az_plot(:,2));
+else
+    mic_az = [];
+    az_x = [];
+    az_y = [];
+end
+
 % Plot elevation polar circle
 axes(handles.axes_bp_contour);
 cla(handles.axes_bp_contour,'reset');
-polar(el_plot(:,1),el_plot(:,2),'.-');
+if ~isempty(el_x)
+    pp = polar(el_plot(:,1),el_plot(:,2),'.-');
+    set(pp,'markersize',20);
+    hold on
+    text(el_x,el_y,{num2str(mic_el(:))});
+else
+    polar(0,0,'.-');
+end
 title('Elevation');
 % ff = findall(gca,'type','text');
 % t = strtrim(get(ff,'String'));
@@ -73,7 +104,14 @@ title('Elevation');
 % Plot azimuth polar circle
 axes(handles.axes_bp);
 cla(handles.axes_bp,'reset');
-polar(az_plot(:,1),az_plot(:,2),'.-');
+if ~isempty(az_x)
+    pp = polar(-az_plot(:,1),az_plot(:,2),'.-');  % flip az +/-
+    set(pp,'markersize',20);
+    hold on
+    text(az_x,-az_y,num2str(mic_az(:)),'color','r');  % flip az +/-
+else
+    polar(0,0,'.-');
+end
 title('Azimuth');
 
 
